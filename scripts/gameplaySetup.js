c37799a7edb16e0d360586b6fa753a1d52d8fcdf
maximtsai <maximtsai@yahoo.com>
2024-02-23 14:37:08 -0800

    scaling

--- scripts/gameplaySetup.js
+++ scripts/gameplaySetup.js
@@ -95,7 +95,7 @@ function setupLoadingBar(scene) {
             handleAchievements(achievements);
             loadingBar.scaleX = 100 + extraLoadingBarLength;
             if (!gameVars.showedCreditsSpook) {
-                let eye = PhaserScene.add.image(635, gameConsts.halfHeight - 275, 'lowq', 'spook4.png').setDepth(0).setAlpha(0.03).setScale(1.97);
+                let eye = PhaserScene.add.image(gameConsts.width - 130, gameConsts.halfHeight - 275, 'lowq', 'spook4.png').setDepth(0).setAlpha(0.03).setScale(1.97);
                 setTimeout(() => {
                     eye.setScale(2, 1.9).setAlpha(0.15);
                     setTimeout(() => {
@@ -320,7 +320,7 @@ function clickCredits() {
 function closeCredits() {
     if (!gameVars.showedCreditsSpook) {
         if (Math.random() < 0.3) {
-            let eye = PhaserScene.add.image(gameConsts.halfWidth + 260, gameConsts.halfHeight - 255, 'lowq', 'spook4.png').setDepth(0).setAlpha(0.15).setScale(3);
+            let eye = PhaserScene.add.image(gameConsts.width - 130, gameConsts.halfHeight - 255, 'lowq', 'spook4.png').setDepth(0).setAlpha(0.15).setScale(3);
             setTimeout(() => {
                 eye.destroy();
             }, 20)
@@ -1494,7 +1494,7 @@ function runIntroSequence() {
     let thunderSfx = playSound('thunder', 0.8);
 
     if (!gameVars.showedCreditsSpook) {
-        let eye = PhaserScene.add.image(gameConsts.halfWidth + 260, gameConsts.halfHeight - 255, 'lowq', 'spook4.png').setDepth(0).setAlpha(0.1).setScale(3);
+        let eye = PhaserScene.add.image(gameConsts.width - 130, gameConsts.halfHeight - 255, 'lowq', 'spook4.png').setDepth(0).setAlpha(0.1).setScale(3);
         setTimeout(() => {
             eye.setScale(3.1).setAlpha(0.15);
             setTimeout(() => {
